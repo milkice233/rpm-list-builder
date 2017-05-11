d8e73613607eb39e4b8b4e7c74b940e5afeb1207
Jan Staněk <jstanek@redhat.com>
2017-05-11 16:15:16 +0200

    Replace the app module

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,77 +1,29 @@
+"""Test argument parsing"""
+
 import os
-from unittest import mock
 
-from rpmlb.app import Application
-
-
-def test_init():
-    app = Application()
-    assert app
+from rpmlb.cli import run
 
 
-def test_run_calls_run_internally():
-    app = Application()
-    app.run_internally = mock.MagicMock(return_value=True)
-    app.run()
-    assert app.run_internally.called
+def test_parse_argv_no_options(tmpdir):
+    """Tests proper default values of the CLI"""
 
+    RECIPE_FILE = tmpdir.join('ror.yml')
+    RECIPE_NAME = 'rh-ror50'
 
-def test_run_returns_true_on_success():
-    app = Application()
-    app.run_internally = lambda *args: None
-    assert app.run()
+    # Prepare environment
+    RECIPE_FILE.write('')
 
+    # Parse the arguments
+    with tmpdir.as_cwd():
+        argv = list(map(str, [RECIPE_FILE, RECIPE_NAME]))
+        args = run.make_context('rpmlb', argv).params
 
-def test_run_returns_false_on_exception():
-    app = Application()
+    current_dir = os.path.abspath(os.getcwd())
 
-    def run_internally(*args):
-        raise RuntimeError('test')
-
-    app.run_internally = run_internally
-    assert not app.run()
-
-
-def test_parse_argv_no_options():
-    app = Application()
-    args = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50'])
-    assert args
-    assert args.recipe_file == '/tmp/ror.yml'
-    assert args.recipe_id == 'rh-ror50'
-    assert args.build == 'dummy'
-    assert args.download == 'none'
-    assert args.branch is None
-    current_dir = os.getcwd()
-    assert args.source_directory == current_dir
-
-
-def test_parse_argv_build():
-    app = Application()
-    args = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                           '--build', 'mock'])
-    assert args
-    assert args.build == 'mock'
-
-
-def test_parse_argv_download():
-    app = Application()
-    args = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                           '--download', 'rhpkg'])
-    assert args
-    assert args.download == 'rhpkg'
-
-
-def test_parse_branch():
-    app = Application()
-    args = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                           '--branch', 'rhscl-2.3-rh-ror42-rhel-7'])
-    assert args
-    assert args.branch == 'rhscl-2.3-rh-ror42-rhel-7'
-
-
-def test_parse_source_directory():
-    app = Application()
-    args = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                           '--source-directory', '/tmp/foo'])
-    assert args
-    assert args.source_directory == '/tmp/foo'
+    assert args['recipe_file'] == str(RECIPE_FILE), 'Wrong recipe file path'
+    assert args['recipe_name'] == str(RECIPE_NAME), 'Wrong recipe name'
+    assert args['build'] == 'dummy', 'Wrong builder name'
+    assert args['download'] == 'none', 'Wrong downloader name'
+    assert args['branch'] is None, 'Superfluous branch'
+    assert args['source_directory'] == current_dir
